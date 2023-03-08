47352d9e25d594174f38e1b1ac07acfdf7e24995
chewyang <owenzhouyang98@gmail.com>
2023-03-09 01:49:25 +0800

    corrected docker-compose and support host view

--- my_app.py
+++ my_app.py
@@ -94,4 +94,5 @@ def logout():
 if __name__=="__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True) #allow dynamic changes
+    app.run(debug=True, host='0.0.0.0') #allow dynamic changes
+
